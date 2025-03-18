d3d450bf430c1d9d2ed2405f10b19bdad6281314
Eazicode <mistereze16@gmail.com>
2025-03-18 21:09:59 +0100

    this is react router dom

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,12 +1,16 @@
 import NavLinks from "./NavLinks";
+import {Link} from 'react-router-dom'
+
 
 const NavBar = () => {
   return ( 
     <>
       <nav className='flex justify-between px-5 md:px-10 lg:px-30 items-center pb-30'>
-        <div>
-          <img className='w-35 md:w-45' src="../logo.svg" alt="" />
-        </div>
+        <Link>
+          <div>
+            <img className='w-35 md:w-45' src="../logo.svg" alt="" />
+          </div>
+        </Link>
 
         <NavLinks  className={'hidden md:flex gap-5 font-bold text-white text-sm'}/>
 
